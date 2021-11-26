3f6986fee07c1901da6b6b8cffb372abaaf49924
4501104220 <thangtrantt@hotmail.com>
2021-11-26 16:18:25 +0700

    update: foder Test Login HCMUE (online.hcmue.edu.vn)

---
+++ Test Login HCMUE/Object Repository/Page_UP Portal  HoChiMinh University of Pedagogy/table_ng nhp                               _360d0c.rs
@@ -0,0 +1,152 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>table_ng nhp                               _360d0c</name>
+   <tag></tag>
+   <elementGuidId>99beeb10-80b3-434f-83e4-f2bfb4281f7b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//table[@id='table6']/tbody/tr/td[2]/table</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>table</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+            
+                
+                    
+                        
+                            
+                                Đăng nhập
+                            
+                        
+                    
+                
+                
+                    
+                        
+                            
+                                
+                                    Sinh viên
+                                
+                                    Giảng viên
+                                
+                                    Nhân viên
+                                
+                                    Ban lãnh đạo
+                            
+                        
+                    
+                
+                
+                      Mật mã truy cập không chính xác.
+
+                
+                
+                    
+                        
+                            
+                                
+                                    Tên đăng nhập:
+                                
+                                    
+                                
+                            
+                            
+                                
+                                    Mật mã: 
+                                
+                                    
+                            
+                            
+                                
+                                    
+                                        
+                                            
+                                                Ghi nhớ lần đăng nhập sau
+                                        
+                                    
+                                
+                            
+                            
+                             
+                                
+                                
+                                
+                                        
+                                            
+                                            
+                                                Số lượng truy cập : 219/559092 
+                                            
+                                               
+                                            
+                                                
+                                            
+                                        
+                                    
+                                
+                            
+                            
+                        
+                    
+                
+            
+        
+    
+</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;table6&quot;)/tbody[1]/tr[1]/td[2]/table[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//table[@id='table6']/tbody/tr/td[2]/table</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng nhập'])[1]/following::table[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hướng dẫn'])[1]/following::table[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//tr[5]/td/table/tbody/tr/td[2]/table</value>
+   </webElementXpaths>
+</WebElementEntity>
